1e3d1bc0a651db25693f7e197cc58ec483c14cb8
Prueba <alexisvarela652@gmail.com>
2025-03-14 17:30:33 -0600

    Cree el servidor y mude todo a vistas ejs

---
+++ server/app.js
@@ -0,0 +1,28 @@
+// Requerimientos
+const express = require('express');
+const app = express();
+const env = require('dotenv').config();
+const path = require("path");
+
+// Variables que tienen que ver 
+const PORT = process.env.PORT || 3000 ;
+
+//Configurar el motor de plantillas los htmls y archivos ejs.
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+// Para que busque los archivos estaticos en la carpeta public
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+// Definición de puerto
+app.listen(PORT, () => {
+    console.log('Ya jalo TU', PORT);
+
+});
+
+// Rutas
+app.get('/', (req, res)=>{
+    // res.sendFile(path.join(__dirname, 'views', 'principal.html'));;
+    res.render('carrucel');
+});
