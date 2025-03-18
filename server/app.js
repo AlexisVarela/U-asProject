347014abe0f57ce5909cecb3127b6952fcbd5e08
EricM-G-V <al24320332@utcj.edu.mx>
2025-03-18 11:27:24 -0600

    Modifique el app.js

--- server/app.js
+++ server/app.js
@@ -18,17 +18,13 @@ app.use(session({
 }));
 
 
-// Variables que tienen que ver 
 const PORT = process.env.PORT || 3000 ;
-
-
 
 //Configurar el motor de plantillas los htmls y archivos ejs.
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 // Para que busque los archivos estaticos en la carpeta public
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 
 // Definición de puerto
@@ -87,6 +83,7 @@ app.post('/login', (req, res) => {
         res.redirect('/');
     });
 });
+
 
 // Ruta para cerrar sesión
 app.get('/logout', (req, res) => {
